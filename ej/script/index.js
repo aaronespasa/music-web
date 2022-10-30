5cd679064fe7c5844bb476815f7d0686f0bca705
Carlos Iborra Llopis <charlie.iborra@gmail.com>
2022-10-30 15:00:47 +0100

    Varias novedades: - cuenta: esta doblado, hacer que funcione el botón de editar perfil - perfil: falta por modificar

--- ej/script/index.js
+++ ej/script/index.js
@@ -7,6 +7,9 @@ const loginForm = document.getElementById("login-form");
 const navbarLinks = document.getElementById("navbar-links-container")
 const selectImageInput = document.getElementById("input-file");
 const selectImageButton = document.getElementById("select-image-button")
+const profileName = document.getElementById("profile-title");
+const profileData = document.getElementById("profile-body");
+
 
 const USERNAME_MIN_LENGTH = 5;
 const PASSWORD_MIN_LENGTH = 8;
@@ -121,6 +124,7 @@ const login = (event) => {
 }
 
 
+
 const setNavbarLinks = () => {
     const isAuthenticated = localStorage.getItem("isAuthenticated");
 
@@ -134,11 +138,19 @@ const setNavbarLinks = () => {
         window.location.href = 'index.html';
     `
 
+    // Esto hay que crearlo como la imagen del usuario y si se le da click, que te muestre las siguientes opciones
     if (isAuthenticated === "true") {
         navbarLinks.innerHTML = `
+            <button onclick="location.href='account.html'" class="sign-log-in-button sign-in-button">
+                Cuenta
+            </button>
+            <button onclick="location.href='profile.html'" class="sign-log-in-button sign-in-button">
+                Perfil
+            </button>
             <button onclick="${logoutFunction}" class="sign-log-in-button sign-in-button">
                 Cerrar Sesión
             </button>`;
+
     } else {
         navbarLinks.innerHTML = `
             <button onclick="location.href='signup.html'" class="sign-log-in-button sign-in-button">
@@ -148,6 +160,147 @@ const setNavbarLinks = () => {
                 Iniciar Sesión
             </button>`;
     }
+
+
+    // ! Añadimos la pagína del perfil del usuario
+    if (isAuthenticated === "true" && window.location.href.includes("profile.html")) {
+        const user = JSON.parse(localStorage.getItem("user"));
+        const name = user.name, surname = user.surname;
+        
+        profileName.innerHTML = `
+        <h2 class="profile-name">
+            Perfil de ${name} ${surname}
+        </h2>`;
+
+        // set the profile data
+        const username = user.username, email = user.email, birthdate = user.birthdate, image = user.image;
+        profileData.innerHTML = `
+        <div class="profile-data">
+            <img src="${image}" id="profile-image" alt="profile-image">
+        </div>
+        <div class="profile-data">
+            <p id="profile-data-title">Nombre de usuario</p>
+            <p id="profile-data-info">@${username}</p>
+            <p id="profile-data-title">Correo electrónico</p>
+            <p id="profile-data-info">${email}</p>
+            <p id="profile-data-title">Fecha de nacimiento</p>
+            <p id="profile-data-info">${birthdate}</p>
+        </div>
+        <hr>
+        <div class="profile-extra">
+            <p id="profile-extra-title">Artistas más escuchados</p>
+            <div class="profile-extra-artists" id="profile-extra-artists">
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+            </div>
+            <p id="profile-extra-title">Canciones más escuchadas</p>
+            <div class="profile-extra-artists" id="profile-extra-artists">
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+            </div>
+            <p id="profile-extra-title">Canciones favoritas</p>
+            <div class="profile-extra-artists" id="profile-extra-artists">
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+            </div>
+            <p id="profile-extra-title">Canciones del usuario</p>
+            <div class="profile-extra-artists" id="profile-extra-artists">
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+            </div>
+            <p id="profile-extra-title">Seguidores</p>
+            <div class="profile-extra-artists" id="profile-extra-artists">
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+                <div class="artist-profile">
+                    <img src="images/pimp.jpeg" alt="artist1" class="artist-img">
+                    <p class="artist-name">Pimp Flaco</p>
+                </div>
+            </div>
+        </div>`;}
+    
+
+    // ! Añadimos la página de la cuenta
+    if (isAuthenticated === "true" && window.location.href.includes("account.html")) {
+        const user = JSON.parse(localStorage.getItem("user"));
+        const name = user.name, surname = user.surname;
+        
+        profileName.innerHTML = `
+        <h2 class="profile-name">
+            Cuenta de ${name} ${surname}
+        </h2>`;
+
+        // set the profile data
+        const username = user.username, email = user.email, birthdate = user.birthdate, image = user.image;
+        
+        profileData.innerHTML = `
+        <div class="profile-data">
+            <img src="${image}" id="profile-image" alt="profile-image">
+        </div>
+        <div class="profile-data">
+            <p id="profile-data-title">Nombre de usuario</p>
+            <p id="profile-data-info">@${username}</p>
+            <p id="profile-data-title">Correo electrónico</p>
+            <p id="profile-data-info">${email}</p>
+            <p id="profile-data-title">Fecha de nacimiento</p>
+            <p id="profile-data-info">${birthdate}</p>
+        </div>
+        <hr>`;
+        
+        // Añadimos el botón de editar perfil
+        profileData.innerHTML += `
+        <div class="profile-data">
+            <button onclick="location.href='edit-profile.html'" class="sign-log-in-button">
+                Editar Perfil
+            </button>
+        </div>`;}
+
 }
 
 if (signUpForm !== null) {
